cdd2fe5e1c0a15ee1116469ab9196d4c45c4d0f5
Anton <antonkhazin2006@gmail.com>
2022-05-27 20:46:51 +0300

    addSVG

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-
-
-
-
-
-
+const WHEEL = document.querySelector(".wheel");
+console.log(1);
+WHEEL.insertAdjacentHTML("beforeend","") 
+/*<svg width="403" height="567" viewBox="0 0 403 567" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M284.196 0.353546L1.35356 283.196M284.11 566.377L1.26685 283.535M284.463 1.18115C373.463 92.1811 498.063 332.381 284.463 565.181" stroke="black"/>
+</svg>
+*/
 
