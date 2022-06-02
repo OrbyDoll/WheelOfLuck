ce426525af6b87885e3f32fb7628a645acf4cf4e
tosha <antonkhazin2006@gmail.com>
2022-06-02 20:48:06 +0300

    wheelDone

--- main.js
+++ main.js
@@ -1,17 +1,50 @@
 const WHEEL = document.querySelector(".wheel");
-
+let degres = 1;
+function generateColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+let rotation = setInterval(() => {
+  WHEEL.style.transform = `rotateZ(${degres}deg)`;
+  degres++;
+  if (degres == 361) {
+    clearInterval(rotation);
+  }
+}, 1);
 WHEEL.insertAdjacentHTML(
   "beforeend",
-  `<svg viewBox="-400 -100 800 800" fill="none" xmlns="http://www.w3.org/2000/svg">
+  `<svg viewBox="-400 -400 800 800" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
-    class="wheel-part"
-      d="M284.196 0.353546L1.35356 283.196M284.11 566.377L1.26685 283.535M284.463 1.18115C373.463 92.1811 498.063 332.381 284.463 565.181"
+      fill=${generateColor()}
+      style = "transform: rotateZ(90deg);"
+      class="wheel-part"
+      d="M400 0C400 52.5287 389.654 104.543 369.552 153.073C349.45 201.604 319.986 245.699 282.843 282.843C245.699 319.986 201.604 349.45 153.073 369.552C104.543 389.654 52.5287 400 -1.74846e-05 400L0 0H400Z"
       stroke="black"
+      stroke-width="2"
     />
     <path viewBox="0 0 800 800"
+      fill=${generateColor()}
+      style = "transform: rotateZ(180deg);"
       class="wheel-part"
-      d="M284.196 0.353546L1.35356 283.196M284.11 566.377L1.26685 283.535M284.463 1.18115C373.463 92.1811 498.063 332.381 284.463 565.181"
+      d="M400 0C400 52.5287 389.654 104.543 369.552 153.073C349.45 201.604 319.986 245.699 282.843 282.843C245.699 319.986 201.604 349.45 153.073 369.552C104.543 389.654 52.5287 400 -1.74846e-05 400L0 0H400Z"
       stroke="black"
+      stroke-width="2"
+    />
+    <path viewBox="0 0 800 800"
+      fill=${generateColor()}
+      style = "transform: rotateZ(270deg);"
+      class="wheel-part"
+      d="M400 0C400 52.5287 389.654 104.543 369.552 153.073C349.45 201.604 319.986 245.699 282.843 282.843C245.699 319.986 201.604 349.45 153.073 369.552C104.543 389.654 52.5287 400 -1.74846e-05 400L0 0H400Z"
+      stroke="black"
+      stroke-width="2"
+    />
+    <path viewBox="0 0 800 800"
+      fill=${generateColor()}
+      style = "transform: rotateZ(360deg);"
+      class="wheel-part"
+      d="M400 0C400 52.5287 389.654 104.543 369.552 153.073C349.45 201.604 319.986 245.699 282.843 282.843C245.699 319.986 201.604 349.45 153.073 369.552C104.543 389.654 52.5287 400 -1.74846e-05 400L0 0H400Z"
+      stroke="black"
+      stroke-width="2"
     />
   </svg>`
 );
+
